0c7949dbf5fd1a9f11f1464c11ec5ace6a2eb94c
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-04-25 22:49:37 +0300

    add Statistics and basic styles

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from 'components/Statistics/Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>Upload stats</h2>}
+      <ul className={css.statList}>
+        {stats.map(stat => (
+          <li className={css.item} key={stat.id}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stast: PropTypes.exact({
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  }),
+};
