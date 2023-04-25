d27ab51d07dc63ae7f3349cbae9100c034c6b81b
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-04-26 01:40:29 +0300

    add PropTypes to TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import css from 'components/TransactionHistory/TransactionHistory.module.css';
+
 export const TransactionHistory = ({ items }) => {
   return (
     <table className={css.transactionHistory}>
@@ -20,4 +22,15 @@ export const TransactionHistory = ({ items }) => {
       </tbody>
     </table>
   );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
 };
