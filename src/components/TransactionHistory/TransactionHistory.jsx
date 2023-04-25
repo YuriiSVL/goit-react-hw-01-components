040eca77e6a15879054f0db8d03d5b6a8e32c442
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-04-26 01:36:18 +0300

    add TransactionHistory and basic styles

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,23 @@
+import css from 'components/TransactionHistory/TransactionHistory.module.css';
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead className={css.head}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody className={css.body}>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
